c51993351aad5d1eb41b8c9c23c5426f2b6db915
Youngrong <youngrong@stibee.com>
2021-07-16 20:10:34 +0900

    Updates

---
+++ precache-manifest.071d160568eef7cfd02c1fdf1633d032.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd13d202a9bcf62d91415d5e2858d647",
+    "url": "/stibeeLab-onBoard/index.html"
+  },
+  {
+    "revision": "d1f5ee04158f4f59a66f",
+    "url": "/stibeeLab-onBoard/static/css/main.aa0dfed3.chunk.css"
+  },
+  {
+    "revision": "c5a4f797aae19ff88932",
+    "url": "/stibeeLab-onBoard/static/js/2.8e9f0561.chunk.js"
+  },
+  {
+    "revision": "c3ccefa8c26d9c4526d0730515716d1c",
+    "url": "/stibeeLab-onBoard/static/js/2.8e9f0561.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1f5ee04158f4f59a66f",
+    "url": "/stibeeLab-onBoard/static/js/main.6d98d8fb.chunk.js"
+  },
+  {
+    "revision": "e1043c3a5eeea29fa2fb",
+    "url": "/stibeeLab-onBoard/static/js/runtime-main.679920ed.js"
+  },
+  {
+    "revision": "3d89937c0ef43d8933328b35efd6d878",
+    "url": "/stibeeLab-onBoard/static/media/check.3d89937c.svg"
+  },
+  {
+    "revision": "ee27b3f21e6f4e90c5a203f08fc88e0e",
+    "url": "/stibeeLab-onBoard/static/media/circle.ee27b3f2.svg"
+  },
+  {
+    "revision": "96c1d6966b029c840dc4efd6eeed37b2",
+    "url": "/stibeeLab-onBoard/static/media/curved.96c1d696.svg"
+  },
+  {
+    "revision": "2549d82c1ae94ab502cb50575974a0c2",
+    "url": "/stibeeLab-onBoard/static/media/rect.2549d82c.svg"
+  },
+  {
+    "revision": "573388b926312d1e050a9b31eb2d5681",
+    "url": "/stibeeLab-onBoard/static/media/save.573388b9.svg"
+  },
+  {
+    "revision": "29617c6798e3dfd215a292761861edf7",
+    "url": "/stibeeLab-onBoard/static/media/select.29617c67.svg"
+  },
+  {
+    "revision": "53bb421c4a202d4aef265232892d60fe",
+    "url": "/stibeeLab-onBoard/static/media/text.53bb421c.svg"
+  }
+]);
